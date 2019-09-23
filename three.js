7fc772052509f887b236c08e927c59c1e6838a40
Kristoffer Eide <kristoffer@maksimer.no>
2019-09-23 09:49:25 +0200

    changelog and codestyle fixes

--- three.js
+++ three.js
@@ -1,2 +1,3 @@
-numbers.pop(); //  pop modifies the array. deletes the last index
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers.pop();
 console.log(numbers);
--- two.js
+++ two.js
@@ -1,2 +1,2 @@
-var numbers = [1,2,3,4,5,6,7,8,9,10];
-console.log(numbers.slice(4,5)); // slice does not modify the arrray
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers.slice(4, 5)); // slice does not modify the arrray
